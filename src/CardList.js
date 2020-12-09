552083e59c739a1b91ba50c0be297cefb8beba44
Andrew Sloan <sloancodes@gmail.com>
2020-12-08 20:08:35 -0700

    ux set

--- src/CardList.js
+++ src/CardList.js
@@ -1,27 +1,32 @@
 import React from "react";
-import { Card, Button } from "semantic-ui-react";
+import { Button, Card, Icon } from "semantic-ui-react";
 
 const CardList = ({ cards }) => {
   return  cards.map( card => {
   return (
+  
+    <>
   <Card
+    
   key={card.id}
   href='#card-example-link-card'
   header='Question'
   meta='Flash Card'
   description= {card.question}
   
-  
   />
+<Button icon='edit'  />
+<Button icon='trash' />
+<Button content="Show Answer"/>
+</>
   
+)},
+)}
   
-  )}
-  
-  )};
 
 
 export default CardList;
-trash alternate
+
 
 
 
